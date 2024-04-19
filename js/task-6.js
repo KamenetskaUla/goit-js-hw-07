0c8d2bb643e0b4eaf352684c43d2b88aece8ec31
KamenetskaUla <152808377+KamenetskaUla@users.noreply.github.com>
2024-04-19 17:53:10 +0300

    add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  input.value = "";
+
+  validateInput(amount) && createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function validateInput(amount) {
+  return amount >= 1 && amount <= 100;
+}
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
